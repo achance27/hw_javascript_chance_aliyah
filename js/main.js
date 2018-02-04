77cd2c56fe92d892333e78167f6ee0635ef28d89
Aliyah Chance <Aliyah@aliyahs-mbp.attlocal.net>
2018-02-04 16:27:00 -0500

    Add U.S. Executive Office recent history

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 // ICE CREAM
 var iceCream = ['coffee', 'salted caramel', 'vanilla',
     'cookies and cream'];
+
+// PRESIDENTS
+var presidents = [
+    {firstName: 'Donald', lastName: 'Trump', termLength: 1, party: 'Republican', yearsOfPresidency: 1},
+    {firstName: 'Barack', lastName: 'Obama', termLength: 2, party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George W.', lastName: 'Bush', termLength: 2, party: 'Republican', yearsOfPresidency: 8},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: 2, party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George H.W.', lastName: 'Bush', termLength: 1, party: 'Republican', yearsOfPresidency: 4}
+];
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
