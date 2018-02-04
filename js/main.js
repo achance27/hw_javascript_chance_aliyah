a92ad54998c673deca5dcc02c981df6531276a13
Aliyah Chance <Aliyah@aliyahs-mbp.attlocal.net>
2018-02-04 16:39:29 -0500

    Add the perfect number length

--- js/main.js
+++ js/main.js
@@ -17,3 +17,17 @@ function howdy() {
     console.log('Aliyah Chance says hi!');
 }
 howdy();
+
+//  NUMBA EXPRESSION
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    } else if (string.length === 7) {
+        console.log('7, what a perfect choice');
+    } else {
+        console.log('I\'m sorry, but that\'s too many to count.');
+    }
+};
+numba('7');
+numba('seventy');
+numba('university');
